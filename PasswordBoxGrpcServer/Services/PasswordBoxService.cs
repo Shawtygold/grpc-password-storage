ef6d754fa278cfb1b8886370d66955548ef87b9f
shawtygold <mylnikovmisha@gmail.com>
2024-07-28 14:08:53 +0500

    Init commit

---
+++ PasswordBoxGrpcServer/Services/PasswordBoxService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace PasswordBoxGrpcServer.Services
+{
+    public class PasswordBoxService : PasswordBox.PasswordBoxBase
+    {
+        private readonly ILogger<PasswordBoxService> _logger;
+        public PasswordBoxService(ILogger<PasswordBoxService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<RegisterUserReply> RegisterUser(RegisterUserRequest request, ServerCallContext context)
+        {
+            return null;
+        }
+    }
+}
