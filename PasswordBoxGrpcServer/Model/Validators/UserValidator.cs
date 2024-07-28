6802e406e8eb38116376296536e9060537b0cbff
shawtygold <mylnikovmisha@gmail.com>
2024-07-28 14:33:14 +0500

    Modify UserValidator.cs

--- PasswordBoxGrpcServer/Model/Validators/UserValidator.cs
+++ PasswordBoxGrpcServer/Model/Validators/UserValidator.cs
@@ -1,6 +1,20 @@
-﻿namespace PasswordBoxGrpcServer.Model.Validators
+﻿using FluentValidation;
+using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Model.Validators
 {
-    public class UserValidator
+    public class UserValidator : AbstractValidator<User>
     {
+        private const int loginMinLength = 2;
+        private const int loginMaxLength = 50;
+        private const int passwordMinLength = 4;
+        private const int passwordMaxLength = 100;
+
+        public UserValidator()
+        {
+            RuleFor(u => u.Id).NotNull().GreaterThan(-1);
+            RuleFor(u => u.Login).NotNull().MinimumLength(loginMinLength).MaximumLength(loginMaxLength);
+            RuleFor(u => u.Password).NotNull().MinimumLength(passwordMinLength).MaximumLength(passwordMaxLength);
+        }
     }
 }
