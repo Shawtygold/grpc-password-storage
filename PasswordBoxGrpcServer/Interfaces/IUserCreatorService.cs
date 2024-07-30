decbe45eb6c51cc434adff745558551e53ffcf0f
shawtygold <mylnikovmisha@gmail.com>
2024-07-30 10:42:55 +0500

    Add UserCreatorService

---
+++ PasswordBoxGrpcServer/Interfaces/IUserCreatorService.cs
@@ -0,0 +1,9 @@
+﻿using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Interfaces
+{
+    public interface IUserCreatorService
+    {
+        User Create(string login, string password);
+    }
+}
---
+++ PasswordBoxGrpcServer/Services/UserCreatorService.cs
@@ -0,0 +1,15 @@
+﻿using PasswordBoxGrpcServer.Interfaces;
+using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Services
+{
+    public class UserCreatorService : IUserCreatorService
+    {
+        public User Create(string login, string password)
+        {
+            // Валидация и создание пользователя
+            User user = new(login, password);
+            return user;
+        }
+    }
+}
