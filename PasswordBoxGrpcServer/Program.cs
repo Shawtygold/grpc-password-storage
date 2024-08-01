3bd7ecdca96632b4800318ed9221cd6515d4cc0e
shawtygold <mylnikovmisha@gmail.com>
2024-08-01 15:16:16 +0500

    Add Dependency Injection

--- PasswordBoxGrpcServer/Program.cs
+++ PasswordBoxGrpcServer/Program.cs
@@ -1,9 +1,18 @@
+using PasswordBoxGrpcServer.Interfaces;
+using PasswordBoxGrpcServer.Interfaces.Repositories;
+using PasswordBoxGrpcServer.Model.AppContext;
+using PasswordBoxGrpcServer.Model.Repositories;
 using PasswordBoxGrpcServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddTransient<ApplicationContext>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserRegistrationService, UserRegistrationService>();
+builder.Services.AddTransient<IUserAuthenticationService, UserAuthenticationService>();
+builder.Services.AddTransient<IUserCreatorService, UserCreatorService>();
 
 var app = builder.Build();
 
