e196f502b07498682c075c73a420a6b44af5af69
shawtygold <mylnikovmisha@gmail.com>
2024-08-01 15:12:50 +0500

    Add UserAuthenticationService.cs

---
+++ PasswordBoxGrpcServer/Interfaces/IUserAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Interfaces
+{
+    public interface IUserAuthenticationService : IDisposable
+    {
+        Task<bool> Authenticate(User user);
+    }
+}
---
+++ PasswordBoxGrpcServer/Services/UserAuthenticationService.cs
@@ -0,0 +1,36 @@
+﻿using PasswordBoxGrpcServer.Interfaces;
+using PasswordBoxGrpcServer.Interfaces.Repositories;
+using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Services
+{
+    public sealed class UserAuthenticationService : IUserAuthenticationService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserAuthenticationService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> Authenticate(User user)
+        {
+            ArgumentNullException.ThrowIfNull(user);
+
+            User? dbUser = await _userRepository.GetByAsync(u => u.Login == user.Login);
+
+            if (dbUser == null)
+                return false;
+
+            if (user.Password != dbUser.Password)
+                return false;
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            _userRepository?.Dispose();
+        }
+    }
+}
