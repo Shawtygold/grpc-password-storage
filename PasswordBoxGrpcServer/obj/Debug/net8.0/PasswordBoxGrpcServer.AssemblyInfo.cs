5c037593a229d3b812a147cb1d3cbf5ca0bc052e
shawtygold <mylnikovmisha@gmail.com>
2024-08-01 15:16:39 +0500

    Commit cache

--- PasswordBoxGrpcServer/obj/Debug/net8.0/PasswordBoxGrpcServer.AssemblyInfo.cs
+++ PasswordBoxGrpcServer/obj/Debug/net8.0/PasswordBoxGrpcServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordBoxGrpcServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b59c83b404d34819abaa3e19d54c530cd0c85b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b291ddceb084c360551569d6584c453c1d5715bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordBoxGrpcServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordBoxGrpcServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
