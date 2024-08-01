e12d8e0505428dd1e296f1d046a10ab3de7e01a2
shawtygold <mylnikovmisha@gmail.com>
2024-08-01 15:15:35 +0500

    Add Authenticate method to PasswordBoxService

--- PasswordBoxGrpcServer/Services/PasswordBoxService.cs
+++ PasswordBoxGrpcServer/Services/PasswordBoxService.cs
@@ -1,28 +1,27 @@
 ﻿using FluentValidation;
 using Grpc.Core;
-using Microsoft.EntityFrameworkCore;
 using PasswordBoxGrpcServer.Interfaces;
-using PasswordBoxGrpcServer.Interfaces.Repositories;
-using PasswordBoxGrpcServer.Model.AppContext;
 using PasswordBoxGrpcServer.Model.Entities;
 using PasswordBoxGrpcServer.Model.Exceptions;
-using PasswordBoxGrpcServer.Model.Repositories;
 
 namespace PasswordBoxGrpcServer.Services
 {
     public sealed class PasswordBoxService : PasswordBox.PasswordBoxBase
     {
         private readonly ILogger<PasswordBoxService> _logger;
-        private readonly IUserRepository _userRepository;
         private readonly IUserRegistrationService _userRegistrationService;
         private readonly IUserCreatorService _userCreatorService;
+        private readonly IUserAuthenticationService _userAuthenticationService;
 
-        public PasswordBoxService(ILogger<PasswordBoxService> logger)
+        public PasswordBoxService(ILogger<PasswordBoxService> logger,
+            IUserRegistrationService registrationService, 
+            IUserCreatorService userCreatorService,
+            IUserAuthenticationService userAuthenticationService)
         {
             _logger = logger;
-            _userRepository = new UserRepository(new ApplicationContext());
-            _userRegistrationService = new UserRegistrationService(_userRepository);
-            _userCreatorService = new UserCreatorService();
+            _userRegistrationService = registrationService;
+            _userCreatorService = userCreatorService;
+            _userAuthenticationService = userAuthenticationService;
         }
 
         public override async Task<RegisterUserReply> RegisterUser(RegisterUserRequest request, ServerCallContext context)
@@ -31,12 +30,10 @@ namespace PasswordBoxGrpcServer.Services
             {
                 User user = _userCreatorService.Create(request.Login, request.Password);
                 await _userRegistrationService.Register(user);
-
-                return new RegisterUserReply { Success = true };
             }
             catch (ValidationException ex)
             {
-                string msg = "User validation error";
+                string msg = "User validation exception";
                 _logger.LogError(AppLogEvents.Exception, ex, msg);
                 throw new RpcException(new Status(StatusCode.InvalidArgument, msg));
             }
@@ -45,6 +42,32 @@ namespace PasswordBoxGrpcServer.Services
                 _logger.LogError(AppLogEvents.Exception, ex, "Default exception");
                 throw new RpcException(new Status(StatusCode.Internal, ex.Message));
             }
+
+            return new RegisterUserReply { Success = true };
+        }
+
+        public override async Task<AuthenticateUserReply> AuthenticateUser(AuthenticateUserRequest request, ServerCallContext context)
+        {
+            bool result = false;
+
+            try
+            {
+                User user = _userCreatorService.Create(request.Login, request.Password);
+                result = await _userAuthenticationService.Authenticate(user);
+            }
+            catch (ValidationException ex)
+            {
+                string msg = "User validation exception";
+                _logger.LogError(AppLogEvents.Exception, ex, msg);
+                throw new RpcException(new Status(StatusCode.InvalidArgument, msg));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(AppLogEvents.Exception, ex.Message, "Default exception");
+                throw new RpcException(new Status(StatusCode.Internal, ex.Message));
+            }
+
+            return new AuthenticateUserReply { Success = result };
         }
     }
 }
