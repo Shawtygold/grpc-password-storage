2b6154620fc0fb143fd521fb5705492698b56e09
shawtygold <mylnikovmisha@gmail.com>
2024-08-02 15:19:34 +0500

    Add PasswordValidator.cs

---
+++ PasswordBoxGrpcServer/Model/Validators/PasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Model.Validators
+{
+    public class PasswordValidator : AbstractValidator<Password>
+    {
+        private const int maxLoginLength = 100;
+
+        public PasswordValidator()
+        {
+            RuleFor(p => p.Id).NotNull().GreaterThan(-1);
+            RuleFor(p => p.Title).NotNull();
+            RuleFor(p => p.Login).NotNull().MaximumLength(maxLoginLength);
+            RuleFor(p => p.PasswordHash).NotNull();
+            RuleFor(p => p.Image).NotNull();
+        }
+    }
+}
