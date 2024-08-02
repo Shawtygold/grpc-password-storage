c05cc3998775c3c3a32e8ea9e01b3ccbb0dc8a07
shawtygold <mylnikovmisha@gmail.com>
2024-08-02 15:18:38 +0500

    Add Password Repository

---
+++ PasswordBoxGrpcServer/Interfaces/Repositories/IPasswordRepository.cs
@@ -0,0 +1,13 @@
+﻿using PasswordBoxGrpcServer.Interfaces.Repositories.Base;
+using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Interfaces.Repositories
+{
+    public interface IPasswordRepository : 
+        IRepositoryGetAll<Password>,
+        IRepositoryAdd<Password>,
+        IRepositoryUpdate<Password>,
+        IRepositoryDelete<Password>, IDisposable
+    {
+    }
+}
---
+++ PasswordBoxGrpcServer/Model/Repositories/PasswordRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordBoxGrpcServer.Interfaces.Repositories;
+using PasswordBoxGrpcServer.Model.AppContext;
+using PasswordBoxGrpcServer.Model.Entities;
+
+namespace PasswordBoxGrpcServer.Model.Repositories
+{
+    public sealed class PasswordRepository : IPasswordRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public PasswordRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Password entity)
+        {
+            await _dbContext.Passwords.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Password entity)
+        {
+            await _dbContext.Passwords.Where(p => p.Id == entity.Id).ExecuteDeleteAsync();           
+        }
+
+        public IEnumerable<Password> GetAll()
+        {
+            return _dbContext.Passwords;
+        }
+
+        public async Task UpdateAsync(Password entity)
+        {
+            _dbContext.Passwords.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+    }
+}
