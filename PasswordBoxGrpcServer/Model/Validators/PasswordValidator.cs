b2534c40c0f4d6427fb4d01be3b7fb44f27b7a40
shawtygold <mylnikovmisha@gmail.com>
2024-08-03 10:35:41 +0500

    Change PasswordValidator.cs

--- PasswordBoxGrpcServer/Model/Validators/PasswordValidator.cs
+++ PasswordBoxGrpcServer/Model/Validators/PasswordValidator.cs
@@ -5,13 +5,12 @@ namespace PasswordBoxGrpcServer.Model.Validators
 {
     public class PasswordValidator : AbstractValidator<Password>
     {
-        private const int maxLoginLength = 100;
-
         public PasswordValidator()
         {
             RuleFor(p => p.Id).NotNull().GreaterThan(-1);
+            RuleFor(p => p.UserLogin).NotNull();
             RuleFor(p => p.Title).NotNull();
-            RuleFor(p => p.Login).NotNull().MaximumLength(maxLoginLength);
+            RuleFor(p => p.LoginHash).NotNull();
             RuleFor(p => p.PasswordHash).NotNull();
             RuleFor(p => p.Image).NotNull();
         }
