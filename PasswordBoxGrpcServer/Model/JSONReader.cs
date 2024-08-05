846f161180af79f4ff917453eb868edbe0cfad2e
shawtygold <mylnikovmisha@gmail.com>
2024-08-05 15:12:11 +0500

    Add JSONReader.cs

---
+++ PasswordBoxGrpcServer/Model/JSONReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace PasswordBoxGrpcServer.Model
+{
+    internal class JSONReader
+    {
+        internal static AESConfigStructure ReadAesConfig()
+        {
+            using (StreamReader streamReader = new("config.json"))
+            {
+                string json = streamReader.ReadToEnd();
+                AESConfigStructure? data = JsonSerializer.Deserialize<AESConfigStructure>(json);
+
+                ArgumentNullException.ThrowIfNull(data);
+
+                return data;
+            }
+        }
+    }
+
+    internal class AESConfigStructure
+    {
+        public string Key { get; set; } = null!;
+        public string IV { get; set; } = null!;
+    }
+}
