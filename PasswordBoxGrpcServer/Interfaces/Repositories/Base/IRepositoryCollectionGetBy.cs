03bb33784b1762dbcc9a250d5463005a6be5a445
shawtygold <mylnikovmisha@gmail.com>
2024-08-08 16:51:14 +0500

    Add IRepositoryCollectionGetBy.cs

---
+++ PasswordBoxGrpcServer/Interfaces/Repositories/Base/IRepositoryCollectionGetBy.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace PasswordBoxGrpcServer.Interfaces.Repositories.Base
+{
+    public interface IRepositoryCollectionGetBy <TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetCollectionBy(Expression<Func<TEntity, bool>> expression);
+    }
+}
