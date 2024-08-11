b965ab2c4c3b5c45a8f7436d50106e6da519e21e
shawtygold <mylnikovmisha@gmail.com>
2024-08-11 16:01:12 +0500

    Remove CreateUser method from IUserService.cs

--- PasswordBoxGrpcServer/Interfaces/Services/Users/IUserService.cs
+++ PasswordBoxGrpcServer/Interfaces/Services/Users/IUserService.cs
@@ -6,7 +6,6 @@ namespace PasswordBoxGrpcServer.Interfaces.Services.Users
     public interface IUserService
     {
         Task<bool> AuthenticateAsync(User user);
-        User CreateUser(string login, string password);
         Task RegisterAsync(User user);
     }
 }
--- PasswordBoxGrpcServer/Services/Users/UserService.cs
+++ PasswordBoxGrpcServer/Services/Users/UserService.cs
@@ -6,25 +6,17 @@ namespace PasswordBoxGrpcServer.Services.Users
     public sealed class UserService : IUserService
     {
         private readonly IUserRegistration _userRegistration;
-        private readonly IUserCreator _userCreator;
         private readonly IUserAuthenticator _userAuthenticator;
 
         public UserService(IUserRegistration registrationService,
-            IUserCreator userCreatorService,
             IUserAuthenticator userAuthenticationService)
         {
             _userRegistration = registrationService;
-            _userCreator = userCreatorService;
             _userAuthenticator = userAuthenticationService;
         }
         public async Task<bool> AuthenticateAsync(User user)
         {
             return await _userAuthenticator.AuthenticateAsync(user);
-        }
-
-        public User CreateUser(string login, string password)
-        {
-            return _userCreator.Create(login, password);
         }
 
         public async Task RegisterAsync(User user)
