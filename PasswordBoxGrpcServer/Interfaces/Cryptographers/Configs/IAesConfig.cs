13d85fb402e47950cedecfc20c7aacd9e3724368
shawtygold <mylnikovmisha@gmail.com>
2024-08-12 14:19:59 +0500

    Add AesConfig.cs

---
+++ PasswordBoxGrpcServer/Interfaces/Cryptographers/Configs/IAesConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace PasswordBoxGrpcServer.Interfaces.Cryptographers.Configs
+{
+    public interface IAesConfig
+    {
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+        public PaddingMode Padding { get; set; }
+        public CipherMode Mode { get; set; }
+        public int BlockSize { get; set; }
+        public int KeySize { get; set; }
+    }
+}
---
+++ PasswordBoxGrpcServer/Model/Cryptographers/Configs/AesConfig.cs
@@ -0,0 +1,28 @@
+﻿using PasswordBoxGrpcServer.Interfaces.Cryptographers.Configs;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordBoxGrpcServer.Model.Cryptographers.Configs
+{
+    public class AesConfig : IAesConfig
+    {
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+        public PaddingMode Padding { get; set; }
+        public CipherMode Mode { get; set; }
+        public int BlockSize { get; set; }
+        public int KeySize { get; set; }
+
+        public AesConfig()
+        {
+            var config = JSONReader.ReadAesConfig();
+
+            Key = Encoding.Default.GetBytes(config.Key);
+            IV = Encoding.Default.GetBytes(config.IV);
+            Padding = (PaddingMode)Convert.ToInt32(config.Padding);
+            Mode = (CipherMode)Convert.ToInt32(config.Mode);
+            BlockSize = Convert.ToInt32(config.BlockSize);
+            KeySize = Convert.ToInt32(config.KeySize);
+        }
+    }
+}
