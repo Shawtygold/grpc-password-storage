f0eaf9fb1a444ad8f0712d3939c9fec1513d34c0
shawtygold <mylnikovmisha@gmail.com>
2024-08-12 14:18:37 +0500

    Commit system files

--- PasswordBoxGrpcServer/obj/Debug/net8.0/PasswordBoxGrpcServer.AssemblyInfo.cs
+++ PasswordBoxGrpcServer/obj/Debug/net8.0/PasswordBoxGrpcServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordBoxGrpcServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9fe0e706c076eca0d26605425631ce46f8a2ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13339b44b265f2e9c0ee1bc2c88a98e222013fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordBoxGrpcServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordBoxGrpcServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
