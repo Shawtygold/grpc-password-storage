d3d488aa76e7e4f0dc6854e009dcbbca73264c47
shawtygold <mylnikovmisha@gmail.com>
2024-08-19 15:31:58 +0500

    Add some unit tests to Testing.cs

--- PasswordBoxGrpcServer.Tests/Testing.cs
+++ PasswordBoxGrpcServer.Tests/Testing.cs
@@ -1,7 +1,40 @@
-﻿namespace PasswordBoxGrpcServer.Tests
-{
-    internal class Testing
-    {
+﻿using Xunit;
+using PasswordBoxGrpcServer.Model;
+using PasswordBoxGrpcServer.Interfaces.Cryptographers;
+using PasswordBoxGrpcServer.Model.Cryptographers;
+using PasswordBoxGrpcServer.Model.Cryptographers.Configs;
+using PasswordBoxGrpcServer.Services.Main;
 
+namespace PasswordBoxGrpcServer.Tests
+{
+    public class Testing
+    {
+        [Fact]
+        public void AesConfigResultNotNull()
+        {
+            //Act
+            var config = JSONReader.ReadAesConfig();
+
+            //Assert
+            Assert.NotNull(config.IV);
+            Assert.NotNull(config.Key);
+            Assert.NotNull(config.Padding);
+            Assert.NotNull(config.Mode);
+        }
+
+        [Fact]
+        public async Task AesBeforeEncryptionAndAfterDecryptionEqual()
+        {
+            //Arrange
+            IEncryptor encryptor = new AESEncryptor(new AesConfig());
+            string data = "Admin";
+
+            //Act
+            string encryptedData = await encryptor.EncryptAsync(data);
+            string decryptedData = await encryptor.DecryptAsync(encryptedData);
+
+            //Assert
+            Assert.Equal(data, decryptedData);
+        }
     }
 }
