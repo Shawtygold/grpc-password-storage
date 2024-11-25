612d55f1528a180a294560fac181f9e807c76c37
shawtygold <mylnikovmisha@gmail.com>
2024-11-25 11:45:39 +0500

    Add EncryptionHelper.cs to AuthorisationService

---
+++ AuthorisationService/Interfaces/Cryptographers/IEncryptionHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthorisationService.Interfaces.Cryptographers
+{
+    public interface IEncryptionHelper
+    {
+        public Task EncryptAsync<TEntity>(IEncryptor encryptor, TEntity entity);
+        public Task DecryptAsync<TEntity>(IEncryptor encryptor, TEntity entity);
+    }
+}
---
+++ AuthorisationService/Model/Cryptographers/EncryptionHelper.cs
@@ -0,0 +1,29 @@
+﻿using AuthorisationService.Interfaces.Cryptographers;
+
+namespace AuthorisationService.Model.Cryptographers
+{
+    public class EncryptionHelper : IEncryptionHelper
+    {
+        public async Task DecryptAsync<TEntity>(IEncryptor encryptor, TEntity entity)
+        {
+            foreach (var property in typeof(TEntity).GetProperties())
+            {
+                if (property.PropertyType != typeof(string) || string.IsNullOrEmpty((string?)property.GetValue(entity)))
+                    continue;
+
+                property.SetValue(entity, await encryptor.DecryptAsync((string)property.GetValue(entity)));
+            }
+        }
+
+        public async Task EncryptAsync<TEntity>(IEncryptor encryptor, TEntity entity)
+        {
+            foreach (var property in typeof(TEntity).GetProperties())
+            {
+                if (property.PropertyType != typeof(string) || string.IsNullOrEmpty((string?)property.GetValue(entity)))
+                    continue;
+
+                property.SetValue(entity, await encryptor.EncryptAsync((string)property.GetValue(entity)));
+            }
+        }
+    }
+}
