421de695b6f0d00923c07c2402d95d0158135790
shawtygold <mylnikovmisha@gmail.com>
2024-12-11 17:51:29 +0500

    Commit other files

---
+++ PasswordService/Enums/EventId/PasswordEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace PasswordService.Enums.EventId
+{
+    public enum PasswordEvent
+    {
+        CreatePassword = 1020,
+        UpdatePassword = 1021,
+        DeletePassword = 1022,
+        GetPasswordsBy = 1023
+    }
+}
