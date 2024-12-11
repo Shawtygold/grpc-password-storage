40a8e07d0424164064ebbfd5ea67940c5ddcafeb
shawtygold <mylnikovmisha@gmail.com>
2024-12-11 17:46:54 +0500

    Add EncryptionHelper.cs

---
+++ PasswordService/Model/Cryptographers/EncryptionHelper.cs
@@ -0,0 +1,29 @@
+﻿using PasswordService.Interfaces.Cryptographers;
+
+namespace PasswordService.Model.Cryptographers
+{
+    public class EncryptionHelper : IEncryptionHelper
+    {
+        public async Task DecryptAsync<TEntity>(IEncryptor encryptor, TEntity entity)
+        {
+            foreach (var property in typeof(TEntity).GetProperties())
+            {
+                if (property.PropertyType != typeof(string) || string.IsNullOrEmpty((string?)property.GetValue(entity)))
+                    continue;
+
+                property.SetValue(entity, await encryptor.DecryptAsync((string)property.GetValue(entity)));
+            }
+        }
+
+        public async Task EncryptAsync<TEntity>(IEncryptor encryptor, TEntity entity)
+        {
+            foreach (var property in typeof(TEntity).GetProperties())
+            {
+                if (property.PropertyType != typeof(string) || string.IsNullOrEmpty((string?)property.GetValue(entity)))
+                    continue;
+
+                property.SetValue(entity, await encryptor.EncryptAsync((string)property.GetValue(entity)));
+            }
+        }
+    }
+}
