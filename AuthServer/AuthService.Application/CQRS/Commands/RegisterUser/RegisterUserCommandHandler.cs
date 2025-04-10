745916068eee1478da4f54afb1a297595bcd3dee
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 09:53:35 +0500

    Add EventSourcingRepository to RegisterUserCommandHandler

--- AuthServer/AuthService.Application/CQRS/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ AuthServer/AuthService.Application/CQRS/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,30 +1,29 @@
 ﻿using AuthService.Application.Abstractions.Mappers;
+using AuthService.Application.Abstractions.Repositories;
 using AuthService.Domain.Entities;
 using AuthService.Domain.Events;
-using Marten;
 
 namespace AuthService.Application.CQRS.Commands.RegisterUser
 {
     public class RegisterUserCommandHandler
     {
-        private readonly ICommandMapper _commandMapper;
-        private readonly IDocumentStore _documentStore;
+        private readonly ICommandEventMapper _commandEventMapper;
+        private readonly IEventSourcingRepository<UserAggregate> _writeRepository;
 
-        public RegisterUserCommandHandler(IDocumentStore documentStore, ICommandMapper commandMapper)
+        public RegisterUserCommandHandler(IEventSourcingRepository<UserAggregate> writeRepository, ICommandEventMapper commandEventMapper)
         {
-            _documentStore = documentStore;
-            _commandMapper = commandMapper;
+            _writeRepository = writeRepository;
+            _commandEventMapper = commandEventMapper;
         }
 
-        public async Task<Guid> HandleAsync(RegisterUserCommand message)
+        public async Task<Guid> HandleAsync(RegisterUserCommand command)
         {
-            UserRegistered registered = await _commandMapper.ToUserRegisteredEvent(message);
+            UserRegistered registeredEvent = _commandEventMapper.Map(command);
+            UserAggregate user = new();
+            user.Apply(registeredEvent);
 
-            using var session = _documentStore.LightweightSession();
-            session.Events.StartStream<User>(registered.Id, registered);
-            await session.SaveChangesAsync();
-
-            return registered.Id;
+            await _writeRepository.SaveAsync(user);
+            return user.Id;
         }
     }
 }
