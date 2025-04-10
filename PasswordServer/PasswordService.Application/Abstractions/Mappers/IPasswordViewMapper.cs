be843f892e5a2b6ad59a5a2de73bb64218a1259b
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 10:31:48 +0500

    feat: add PasswordViewMapper

---
+++ PasswordServer/PasswordService.Application/Abstractions/Mappers/IPasswordViewMapper.cs
@@ -0,0 +1,10 @@
+﻿using PasswordService.Application.DTO;
+using PasswordService.Domain.Entities;
+
+namespace PasswordService.Application.Abstractions.Mappers
+{
+    public interface IPasswordViewMapper
+    {
+        PasswordDTO Map(PasswordView passwordView);
+    }
+}
---
+++ PasswordServer/PasswordService.Application/Mappers/PasswordViewMapper.cs
@@ -0,0 +1,23 @@
+﻿using PasswordService.Application.Abstractions.Mappers;
+using PasswordService.Application.DTO;
+using PasswordService.Domain.Entities;
+
+namespace PasswordService.Application.Mappers
+{
+    public class PasswordViewMapper : IPasswordViewMapper
+    {
+        public PasswordDTO Map(PasswordView passwordView)
+        {
+            return new PasswordDTO(
+                passwordView.Id,
+                passwordView.UserId,
+                passwordView.Title,
+                passwordView.Login,
+                passwordView.EncryptedPassword,
+                passwordView.IconPath,
+                passwordView.Note,
+                passwordView.CreatedAt,
+                passwordView.UpdatedAt);
+        }
+    }
+}
