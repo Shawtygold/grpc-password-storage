f43f80c3c2a90bacb1a2ae39652076da9e8776af
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 09:21:11 +0500

    Add PasswordHasher

---
+++ AuthServer/AuthService.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,32 @@
+﻿using AuthService.Application.Abstractions.Security;
+using System.Security.Cryptography;
+
+namespace AuthService.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        private const int SaltSize = 16;
+        private const int HashSize = 32;
+        private const int Iterations = 100000;
+        private readonly HashAlgorithmName HashAlgorithmName = HashAlgorithmName.SHA512;
+
+        public string Hash(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithmName, HashSize);
+
+            return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            string[] partsOfHash = passwordHash.Split("-");
+            byte[] hash = Convert.FromHexString(partsOfHash[0]);
+            byte[] salt = Convert.FromHexString(partsOfHash[1]);
+
+            byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithmName, HashSize);
+
+            return inputHash.SequenceEqual(hash);
+        }
+    }
+}
