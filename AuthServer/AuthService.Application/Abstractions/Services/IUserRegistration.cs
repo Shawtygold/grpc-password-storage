bfd5231f03d240e3efff8d584cfc00f61476262a
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 09:29:39 +0500

    Delete UserRegistration service

--- AuthServer/AuthService.Application/Abstractions/Services/IUserRegistration.cs
+++
@@ -1,9 +0,0 @@
-﻿using AuthService.Application.DTO;
-
-namespace AuthService.Application.Abstractions.Services
-{
-    public interface IUserRegistration
-    {
-        Task<Guid> RegisterAsync(RegisterUserDTO userDTO);
-    }
-}
