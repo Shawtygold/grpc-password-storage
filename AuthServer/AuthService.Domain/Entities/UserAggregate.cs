0a333feb4f5bbe94daa7f6a478c523d5bfc72c20
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 09:31:20 +0500

    Add UserAggregate and UserView

---
+++ AuthServer/AuthService.Domain/Entities/UserAggregate.cs
@@ -0,0 +1,46 @@
+﻿using AuthService.Domain.Abstractions;
+using AuthService.Domain.Events;
+
+namespace AuthService.Domain.Entities
+{
+    public class UserAggregate : IAggregate
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+
+        private List<object> _uncommitedEvents = new();
+
+        public UserAggregate()
+        {
+
+        }
+
+        public void Apply(object @event)
+        {
+            switch (@event)
+            {
+                case UserRegistered registeredEvent:
+                    {
+                        Apply(registeredEvent);
+                        _uncommitedEvents.Add(registeredEvent);
+                        break;
+                    }
+            }
+        }
+
+        private void Apply(UserRegistered @event)
+        {
+            Id = @event.Id;
+            Login = @event.Login;
+            Email = @event.Email;
+            PasswordHash = @event.PasswordHash;
+            CreatedAt = @event.CreatedAt;
+        }
+
+        public IReadOnlyList<object> GetUncommitedEvents() => _uncommitedEvents.AsReadOnly();
+        public void ClearUncommitedEvents() => _uncommitedEvents.Clear();
+    }
+}
---
+++ AuthServer/AuthService.Domain/Entities/UserView.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Domain.Entities
+{
+    public class UserView
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
