45e96d8bfdc0eb6853093707580fe6543f291661
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 10:08:43 +0500

    Refactor: set up DI

--- AuthServer/AuthService.WebApi/Program.cs
+++ AuthServer/AuthService.WebApi/Program.cs
@@ -1,23 +1,23 @@
-using AESEncryptionLib;
-using AuthService.Application.Abstractions.Encryption;
 using AuthService.Application.Abstractions.Mappers;
+using AuthService.Application.Abstractions.Repositories;
+using AuthService.Application.Abstractions.Security;
 using AuthService.Application.Abstractions.Services;
 using AuthService.Application.CQRS.Commands.RegisterUser;
 using AuthService.Application.Mappers;
+using AuthService.Domain.Entities;
 using AuthService.Domain.Events;
 using AuthService.Infrastructure;
 using AuthService.Infrastructure.Abstractions.Providers;
-using AuthService.Infrastructure.Abstractions.Security;
 using AuthService.Infrastructure.Projections;
 using AuthService.Infrastructure.Providers;
+using AuthService.Infrastructure.Repositories;
 using AuthService.Infrastructure.Security;
 using AuthService.Infrastructure.Services;
-using AuthService.Infrastructure.Validators;
+using AuthService.WebApi.Validators;
 using FluentValidation;
 using GrpcAuthService;
 using Marten;
 using Weasel.Core;
-using WebApi.Validators;
 using Wolverine;
 using Wolverine.FluentValidation;
 
@@ -25,18 +25,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 
+string connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection")
+    ?? throw new InvalidOperationException("PostgreSQL connection not found in configuration");
+
 // Marten
 builder.Services.AddMarten(options =>
 {
-    options.Connection("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=CyfQRh0SGG5g4");
+    options.Connection(connectionString);
     options.UseSystemTextJsonForSerialization();
     options.Events.AddEventType<UserRegistered>();
     options.Projections.Add<UserProjection>(Marten.Events.Projections.ProjectionLifecycle.Inline);
 
     if (builder.Environment.IsDevelopment())
-    {
         options.AutoCreateSchemaObjects = AutoCreate.All;
-    }
 });
 
 // Wolverine
@@ -46,35 +47,33 @@ builder.Host.UseWolverine(options =>
     options.Discovery.IncludeAssembly(typeof(RegisterUserCommand).Assembly);
 });
 
+// Repositories
+builder.Services.AddScoped<IEventSourcingRepository<UserAggregate>, EventSourcingRepository>();
+builder.Services.AddScoped<IProjectionRepository<UserView>, ProjectionRepository>();
+
 // Providers
 builder.Services.AddScoped<IJWTProvider, JWTProvider>();
 
 // Mappers
-builder.Services.AddScoped<IUserMapper, UserMapper>();
-builder.Services.AddScoped<ICommandMapper, CommandMapper>();
+builder.Services.AddScoped<ICommandEventMapper, CommandEventMapper>();
+builder.Services.AddScoped<IUserViewMapper, UserViewMapper>();
 
-// Auth services
-builder.Services.AddScoped<IUserRegistration, UserRegistration>();
+// Services
 builder.Services.AddScoped<IUserAuthenticator, UserAuthenticator>();
 
 // Validators
-builder.Services.AddScoped<IValidator<AesEncryptionConfig>, AesEncryptionConfigValidator>();
 builder.Services.AddScoped<IValidator<AuthenticateUserRequest>, AuthenticateUserRequestValidator>();
 
-// Encryption
-builder.Services.AddScoped<IAesConfigProvider, AesConfigProvider>();
-builder.Services.AddScoped<IAesConfig>(provider => provider.GetRequiredService<IAesConfigProvider>().GetAesConfig());
-builder.Services.AddScoped<IAes, AES>();
-builder.Services.AddScoped<IEncryptor, AesEncryptor>();
+// Hash
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
 // Settings
-builder.Services.Configure<AesSettings>(builder.Configuration.GetRequiredSection(AesSettings.SectionName));
 builder.Services.Configure<JWTSettings>(builder.Configuration.GetRequiredSection(JWTSettings.SectionName));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<WebApi.Services.AuthService>();
+app.MapGrpcService<AuthService.WebApi.Services.AuthService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
