f7f29b2e00d066a4f01d4359ebe7edc8938aa8b1
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 10:45:01 +0500

    refactor: replace extension-method with mapper

--- PasswordServer/PasswordService.Application/CQRS/Queries/GetPasswordsByUserID/GetPasswordsByUserIDQueryHandler.cs
+++ PasswordServer/PasswordService.Application/CQRS/Queries/GetPasswordsByUserID/GetPasswordsByUserIDQueryHandler.cs
@@ -1,6 +1,6 @@
-﻿using PasswordService.Application.Abstractions.Repositories;
+﻿using PasswordService.Application.Abstractions.Mappers;
+using PasswordService.Application.Abstractions.Repositories;
 using PasswordService.Application.DTO;
-using PasswordService.Application.Extensions;
 using PasswordService.Domain.Entities;
 
 namespace PasswordService.Application.CQRS.Queries.GetPasswordsByUserID
@@ -8,17 +8,18 @@ namespace PasswordService.Application.CQRS.Queries.GetPasswordsByUserID
     public class GetPasswordsByUserIDQueryHandler
     {
         private readonly IProjectionRepository<PasswordView> _readRepository;
+        private readonly IPasswordViewMapper _passwordViewMapper;
 
-        public GetPasswordsByUserIDQueryHandler(IProjectionRepository<PasswordView> readRepository)
+        public GetPasswordsByUserIDQueryHandler(IProjectionRepository<PasswordView> readRepository, IPasswordViewMapper passwordViewMapper)
         {
             _readRepository = readRepository;
+            _passwordViewMapper = passwordViewMapper;
         }
 
         public async Task<IEnumerable<PasswordDTO>> Handle(GetPasswordsByUserIDQuery command)
         {
             var passwords = await _readRepository.GetAllAsync(command.UserId);
-
-            return passwords.Select(p => p.ToPasswordDTO());
+            return passwords.Select(_passwordViewMapper.Map);
         }
     }
 }
