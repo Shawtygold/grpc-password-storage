a53207f87a2dcec542ca133b4e2df694f46d0376
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 09:37:07 +0500

    Add ProjectionRepository

---
+++ AuthServer/AuthService.Application/Abstractions/Repositories/IProjectionRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace AuthService.Application.Abstractions.Repositories
+{
+    public interface IProjectionRepository<TAggregate> where TAggregate : class
+    {
+        Task<TAggregate?> GetUserByAsync(Expression<Func<TAggregate, bool>> expression, CancellationToken cancellation = default);
+    }
+}
---
+++ AuthServer/AuthService.Infrastructure/Repositories/ProjectionRepository.cs
@@ -0,0 +1,22 @@
+﻿using AuthService.Application.Abstractions.Repositories;
+using AuthService.Domain.Entities;
+using Marten;
+using System.Linq.Expressions;
+
+namespace AuthService.Infrastructure.Repositories
+{
+    public class ProjectionRepository : IProjectionRepository<UserView>
+    {
+        private readonly IQuerySession _querySession;
+
+        public ProjectionRepository(IQuerySession querySession)
+        {
+            _querySession = querySession;
+        }
+
+        public async Task<UserView?> GetUserByAsync(Expression<Func<UserView, bool>> expression, CancellationToken cancellation = default)
+        {
+            return await _querySession.Query<UserView>().FirstOrDefaultAsync(expression, cancellation);
+        }
+    }
+}
