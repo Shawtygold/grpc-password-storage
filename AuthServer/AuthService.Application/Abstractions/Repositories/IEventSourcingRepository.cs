d228e87e9267f28c3d474ceae6c31984b39c9890
shawtygold <mylnikovmisha@gmail.com>
2025-04-10 09:36:30 +0500

    Add EventSourcingRepository

---
+++ AuthServer/AuthService.Application/Abstractions/Repositories/IEventSourcingRepository.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Domain.Abstractions;
+
+namespace AuthService.Application.Abstractions.Repositories
+{
+    public interface IEventSourcingRepository<TAggregate> where TAggregate : IAggregate, new()
+    {
+        Task SaveAsync(TAggregate aggregate, CancellationToken cancellationToken = default);
+        Task<TAggregate?> GetByIdAsync(Guid aggregateId, CancellationToken cancellationToken = default);
+    }
+}
---
+++ AuthServer/AuthService.Infrastructure/Repositories/EventSourcingRepository.cs
@@ -0,0 +1,35 @@
+﻿using AuthService.Application.Abstractions.Repositories;
+using AuthService.Domain.Entities;
+using Marten;
+
+namespace AuthService.Infrastructure.Repositories
+{
+    public class EventSourcingRepository : IEventSourcingRepository<UserAggregate>
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public EventSourcingRepository(IDocumentStore documentStore)
+        {
+            _documentStore = documentStore;
+        }
+
+        public async Task SaveAsync(UserAggregate aggregate, CancellationToken cancellationToken = default)
+        {
+            IReadOnlyList<object> events = aggregate.GetUncommitedEvents();
+            if (events.Any())
+            {
+                using var session = _documentStore.LightweightSession();
+                session.Events.Append(aggregate.Id, events);
+                await session.SaveChangesAsync(cancellationToken);
+
+                aggregate.ClearUncommitedEvents();
+            }
+        }
+
+        public async Task<UserAggregate?> GetByIdAsync(Guid aggregateId, CancellationToken cancellationToken = default)
+        {
+            using var session = _documentStore.LightweightSession();
+            return await session.Events.AggregateStreamAsync<UserAggregate>(aggregateId, token: cancellationToken);
+        }
+    }
+}
