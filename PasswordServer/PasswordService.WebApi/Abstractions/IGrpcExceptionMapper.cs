bc7fbad3f25a2f768ff8874db89bdf18d2b82a9b
shawtygold <mylnikovmisha@gmail.com>
2025-04-13 10:21:17 +0500

    [passwords] feat: add GrpcExceptionMapper

---
+++ PasswordServer/PasswordService.WebApi/Abstractions/IGrpcExceptionMapper.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Core;
+
+namespace PasswordService.WebApi.Abstractions
+{
+    public interface IGrpcExceptionMapper
+    {
+        RpcException MapException(string domain, string operation, Exception ex);
+    }
+}
---
+++ PasswordServer/PasswordService.WebApi/Mappers/GrpcExceptionMapper.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Grpc.Core;
+using PasswordService.Domain.Exceptions;
+using PasswordService.WebApi.Abstractions;
+using PasswordService.WebApi.Exceptions;
+
+namespace PasswordService.WebApi.Mappers
+{
+    public class GrpcExceptionMapper : IGrpcExceptionMapper
+    {
+        private readonly ILogger<GrpcExceptionMapper> _logger;
+
+        public GrpcExceptionMapper(ILogger<GrpcExceptionMapper> logger)
+        {
+            _logger = logger;
+        }
+
+        public RpcException MapException(string domain, string operation, Exception ex)
+        {
+            switch (ex)
+            {
+                case PasswordNotFoundException notFoundEx: 
+                    {
+
+                        _logger.LogWarning("{Date} {Domain} {Operation} {Status} {Message}", DateTime.Now, domain, operation, "Failed", notFoundEx.Message);
+                        return PasswordRpcExceptions.NotFound(domain, notFoundEx.PasswordId.ToString());                   
+                    }
+                case ValidationException validationEx:
+                    {
+                        _logger.LogWarning("{Date} {Domain} {Operation} {Status} {Message}", DateTime.Now, domain, operation, "Validation Failed", ex.Message);
+                        return PasswordRpcExceptions.InvalidArgumets(validationEx.Errors);
+                    }
+                default:
+                    {
+                        _logger.LogError("{Date} {Domain} {Operation} {Status} {ExMessage}", DateTime.Now, domain, operation, "Failed", ex.Message);
+                        return PasswordRpcExceptions.InternalError(domain, ex);
+                    }
+            }
+        }
+    }
+}
