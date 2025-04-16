d22e87a71640a932c8a862d149c828fd0c76f221
shawtygold <mylnikovmisha@gmail.com>
2025-04-16 12:48:05 +0500

    [auth] refactor: remove command validator and add validators for requests

---
+++ AuthServer/AuthService.WebApi/Validators/Requests/AuthenticateUserRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using GrpcAuthService;
+
+namespace AuthService.WebApi.Validators.Requests
+{
+    public class AuthenticateUserRequestValidator : AbstractValidator<AuthenticateUserRequest>
+    {
+        public AuthenticateUserRequestValidator()
+        {
+            RuleFor(r => r.Login).NotEmpty();
+            RuleFor(r => r.Password).NotEmpty();
+        }
+    }
+}
---
+++ AuthServer/AuthService.WebApi/Validators/Requests/RegisterUserRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using AuthService.Application.CQRS.Queries.CheckUserExists;
+using FluentValidation;
+using GrpcAuthService;
+using System.Net.Mail;
+using Wolverine;
+
+namespace AuthService.WebApi.Validators.Requests
+{
+    public class RegisterUserRequestValidator : AbstractValidator<RegisterUserRequest>
+    {
+        private readonly IMessageBus _messageBus;
+
+        public RegisterUserRequestValidator(IMessageBus messageBus)
+        {
+            _messageBus = messageBus;
+
+            RuleFor(c => c.Login).NotEmpty().MustAsync(async (login, cancellation) => await IsLoginUnique(login, cancellation)).WithMessage("This login already exists");
+            RuleFor(c => c.Email).NotEmpty().Must(IsValidEmail).WithMessage("Invalid email");
+            RuleFor(c => c.Password).NotEmpty();
+        }
+
+        private bool IsValidEmail(string email) => MailAddress.TryCreate(email, out _);
+        private async Task<bool> IsLoginUnique(string login, CancellationToken cancellationToken = default)
+        {
+            CheckUserExistsQuery query = new(login);
+            return !await _messageBus.InvokeAsync<bool>(query, cancellationToken);
+        }
+    }
+}
