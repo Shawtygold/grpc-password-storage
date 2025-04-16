4f6c053993600c354dc55f5f31de60350120ae9e
shawtygold <mylnikovmisha@gmail.com>
2025-04-16 12:51:29 +0500

    [auth] refactor: now GrpcExceptionMapper only does its job

--- AuthServer/AuthService.WebApi/Abstractions/IGrpcExceptionMapper.cs
+++ AuthServer/AuthService.WebApi/Abstractions/IGrpcExceptionMapper.cs
@@ -4,6 +4,6 @@ namespace AuthService.WebApi.Abstractions
 {
     public interface IGrpcExceptionMapper
     {
-        RpcException MapException(string domain, string operation, Exception ex);
+        RpcException MapException(Exception ex);
     }
 }
--- AuthServer/AuthService.WebApi/Mappers/GrpcExceptionMapper.cs
+++ AuthServer/AuthService.WebApi/Mappers/GrpcExceptionMapper.cs
@@ -8,31 +8,25 @@ namespace AuthService.WebApi.Mappers
 {
     public class GrpcExceptionMapper : IGrpcExceptionMapper
     {
-        private readonly ILogger<GrpcExceptionMapper> _logger;
-
-        public GrpcExceptionMapper(ILogger<GrpcExceptionMapper> logger)
-        {
-            _logger = logger;
-        }
-
-        public RpcException MapException(string domain, string operation, Exception ex)
+        public RpcException MapException(Exception ex)
         {
             switch (ex)
             {
-                case UserNotFoundException notFoundEx: 
+                case UserNotFoundException notFoundEx:
                     {
-                        _logger.LogWarning("{Date} {Domain} {Operation} {Status} {Message}", DateTime.Now, domain, operation, "Failed", notFoundEx.Message);
-                        return AuthRpcExceptions.NotFound(domain, notFoundEx.UserLogin);                   
+                        return AuthRpcExceptions.NotFound(notFoundEx.UserLogin);
+                    }
+                case AuthenticationException authEx:
+                    {
+                        return new RpcException(new Status(StatusCode.Unauthenticated, $"{authEx.Message}"));
                     }
                 case ValidationException validationEx:
                     {
-                        _logger.LogWarning("{Date} {Domain} {Operation} {Status} {Message}", DateTime.Now, domain, operation, "Validation Failed", ex.Message);
                         return AuthRpcExceptions.InvalidArgumets(validationEx.Errors);
                     }
                 default:
                     {
-                        _logger.LogError("{Date} {Domain} {Operation} {Status} {ExMessage}", DateTime.Now, domain, operation, "Failed", ex.Message);
-                        return AuthRpcExceptions.InternalError(domain, ex);
+                        return AuthRpcExceptions.InternalError(ex);
                     }
             }
         }
