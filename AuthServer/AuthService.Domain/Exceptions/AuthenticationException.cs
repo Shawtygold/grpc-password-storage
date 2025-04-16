07ad877d5fe5047a4ec4bf5c6c93dc48e6a49924
shawtygold <mylnikovmisha@gmail.com>
2025-04-16 12:48:43 +0500

    [auth] feat: add authentification exception

---
+++ AuthServer/AuthService.Domain/Exceptions/AuthenticationException.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthService.Domain.Exceptions
+{
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException() : base("Invalid login or password") { }
+    }
+}
