527f000446173568079c1b4cb7aadb4809c7fa89
shawtygold <mylnikovmisha@gmail.com>
2025-04-17 17:21:07 +0500

    [passwords] refactor: add PasswordDTOMapper

---
+++ PasswordServer/PasswordService.WebApi/Abstractions/IPasswordDTOMapper.cs
@@ -0,0 +1,10 @@
+﻿using GrpcPasswordService;
+using PasswordService.Application.DTO;
+
+namespace PasswordService.WebApi.Abstractions
+{
+    public interface IPasswordDTOMapper
+    {
+        PasswordResponse ToPasswordResponse(PasswordDTO passwordDTO);
+    }
+}
---
+++ PasswordServer/PasswordService.WebApi/Mappers/PasswordDTOMapper.cs
@@ -0,0 +1,27 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Google.Protobuf;
+using PasswordService.Application.DTO;
+using GrpcPasswordService;
+using PasswordService.WebApi.Abstractions;
+
+namespace PasswordService.WebApi.Mappers
+{
+    public class PasswordDTOMapper : IPasswordDTOMapper
+    {
+        public PasswordResponse ToPasswordResponse(PasswordDTO passwordDTO)
+        {
+            return new PasswordResponse
+            {
+                Id = passwordDTO.Id.ToString(),
+                UserId = passwordDTO.UserId.ToString(),
+                Title = passwordDTO.Title,
+                Login = passwordDTO.Login,
+                EncryptedPassword = ByteString.CopyFrom(passwordDTO.EncryptedPassword),
+                IconPath = passwordDTO.IconPath,
+                Note = passwordDTO.Note,
+                CreatedAt = new Timestamp() { Seconds = passwordDTO.CreatedAt.Second, Nanos = passwordDTO.CreatedAt.Nanosecond },
+                UpdatedAt = new Timestamp() { Seconds = passwordDTO.UpdatedAt.Second, Nanos = passwordDTO.UpdatedAt.Nanosecond }
+            };
+        }
+    }
+}
