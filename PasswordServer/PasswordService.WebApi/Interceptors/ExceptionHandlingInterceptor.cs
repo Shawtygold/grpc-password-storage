8bad0eeeece2c50dd637d51262b6443998d07675
shawtygold <mylnikovmisha@gmail.com>
2025-04-17 17:22:22 +0500

    [passwords] feat: add ExceptionHandlingInterceptor

---
+++ PasswordServer/PasswordService.WebApi/Interceptors/ExceptionHandlingInterceptor.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using PasswordService.WebApi.Abstractions;
+
+namespace PasswordService.WebApi.Interceptors
+{
+    public class ExceptionHandlingInterceptor : Interceptor
+    {
+        private readonly IGrpcExceptionMapper _exceptionMapper;
+
+        public ExceptionHandlingInterceptor(IGrpcExceptionMapper grpcExceptionMapper)
+        {
+            _exceptionMapper = grpcExceptionMapper;
+        }
+
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            try
+            {
+                return await continuation(request, context);
+            }
+            catch (Exception ex) when (ex is TaskCanceledException or OperationCanceledException)
+            {
+                throw new RpcException(new Status(StatusCode.Cancelled, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                throw _exceptionMapper.MapException(ex);
+            }
+        }
+    }
+}
