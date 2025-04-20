4bd486fdbcdcd207ec54ef329625b76f39010e08
shawtygold <mylnikovmisha@gmail.com>
2025-04-20 15:41:57 +0500

    [passwords] feat: add BaseEvent class

---
+++ source/PasswordServer/PasswordService.Domain/Events/BaseEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace PasswordService.Domain.Events
+{
+    public record BaseEvent
+    {
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
--- source/PasswordServer/PasswordService.Domain/Events/Events.cs
+++ source/PasswordServer/PasswordService.Domain/Events/Events.cs
@@ -1,6 +1,6 @@
 ﻿namespace PasswordService.Domain.Events
 {
-    public sealed record PasswordCreated(Guid Id, Guid UserId, string Title, string Login, byte[] EncryptedPassword, string IconPath, string Note, DateTime CreatedAt);
-    public sealed record PasswordUpdated(Guid Id, string Title, string Login, byte[] EncryptedPassword, string IconPath, string Note, DateTime UpdatedAt);
-    public sealed record PasswordDeleted(Guid Id);
+    public sealed record PasswordCreated(Guid Id, Guid UserId, string Title, string Login, byte[] EncryptedPassword, string IconPath, string Note, DateTime CreatedAt) : BaseEvent;
+    public sealed record PasswordUpdated(Guid Id, string Title, string Login, byte[] EncryptedPassword, string IconPath, string Note, DateTime UpdatedAt) : BaseEvent;
+    public sealed record PasswordDeleted(Guid Id) : BaseEvent;
 }
