b245d25a24cd128a1ce89a813bb17c13c60ce431
shawtygold <mylnikovmisha@gmail.com>
2025-04-20 15:34:55 +0500

    [auth] feat: add RegisterUserCommandValidator

---
+++ source/AuthServer/AuthService.Application/Validators/Commands/RegisterUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using AuthService.Application.CQRS.Commands.RegisterUser;
+using FluentValidation;
+
+namespace AuthService.Application.Validators.Commands
+{
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(c => c.Login).NotEmpty();
+            RuleFor(c => c.Password).NotEmpty();
+            RuleFor(c => c.Email).NotEmpty();
+        }
+    }
+}
