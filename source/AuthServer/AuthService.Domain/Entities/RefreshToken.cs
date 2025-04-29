3c282af59cf1dc2b86209442dfb3efba25d25278
shawtygold <mylnikovmisha@gmail.com>
2025-04-29 12:57:30 +0500

    [auth] feat: add RefreshToken entity

---
+++ source/AuthServer/AuthService.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿namespace AuthService.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+        public string Token { get; set; } = null!;
+        public Guid UserId { get; set; }
+        public DateTime ExpirationTime { get; set; }
+
+        public RefreshToken(string token, Guid userId, DateTime expirationTime) : this(default, token, userId, expirationTime) { }
+
+        public RefreshToken(Guid id, string token, Guid userId, DateTime expirationTime)
+        {
+            Id = id;
+            Token = token;
+            UserId = userId;
+            ExpirationTime = expirationTime;
+        }
+    }
+}
