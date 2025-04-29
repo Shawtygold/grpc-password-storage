b7c8d15599d4a4a461c177f799af2e66dfa7c2f0
shawtygold <mylnikovmisha@gmail.com>
2025-04-29 13:09:49 +0500

    [auth] feat: add DeleteRefreshTokensCommandValidator

---
+++ source/AuthServer/AuthService.Application/Validators/Commands/DeleteRefreshTokensCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using AuthService.Application.CQRS.Commands.DeleteRefreshTokens;
+using FluentValidation;
+
+namespace AuthService.Application.Validators.Commands
+{
+    public class DeleteRefreshTokensCommandValidator : AbstractValidator<DeleteRefreshTokensCommand>
+    {
+        public DeleteRefreshTokensCommandValidator()
+        {
+            RuleFor(r => r.UserId).NotEmpty();
+        }
+    }
+}
