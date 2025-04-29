f42c124f084237d92a4410d9d91f4dad78ca8e57
shawtygold <mylnikovmisha@gmail.com>
2025-04-29 13:05:22 +0500

    [auth] feat: add LoginWithRefreshToken method to LoginService

--- source/AuthServer/AuthService.Application/Abstractions/Services/ILoginService.cs
+++ source/AuthServer/AuthService.Application/Abstractions/Services/ILoginService.cs
@@ -1,7 +1,10 @@
-﻿namespace AuthService.Application.Abstractions.Services
+﻿using AuthService.Application.DTO;
+
+namespace AuthService.Application.Abstractions.Services
 {
     public interface ILoginService
     {
-        Task<string> LoginAsync(string login, string password, CancellationToken cancellation = default);
+        Task<LoginResponse> LoginAsync(string login, string password, CancellationToken cancellation = default);
+        Task<LoginResponse> LoginWithRefreshTokenAsync(string refreshToken, CancellationToken cancellation = default);
     }
 }
