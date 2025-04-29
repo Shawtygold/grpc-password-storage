c41976229acab087803d79c29cf4975134baf944
shawtygold <mylnikovmisha@gmail.com>
2025-04-29 13:01:59 +0500

    [auth] feat: add GetRefreshTokenByTokenQuery

---
+++ source/AuthServer/AuthService.Application/CQRS/Queries/GetRefreshToken/GetRefreshTokenByTokenQuery.cs
@@ -0,0 +1,7 @@
+﻿using AuthService.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace AuthService.Application.CQRS.Queries.GetRefreshToken
+{
+    public record GetRefreshTokenByTokenQuery(string Token);
+}
