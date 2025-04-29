115017ab9c3d418c9ad45d8f8fee31fd920be283
shawtygold <mylnikovmisha@gmail.com>
2025-04-29 12:57:58 +0500

    [auth] feat: add ApplicationContext

---
+++ source/AuthServer/AuthService.Infrastructure/AppContext/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using AuthService.Domain.Entities;
+using AuthService.Infrastructure.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace AuthService.Infrastructure.AppContext
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly IOptions<RefreshTokenDbSettings> _options;
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public ApplicationContext(IOptions<RefreshTokenDbSettings> options)
+        {
+            _options = options;
+
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_options.Value.ConnectionString);
+        }
+    }
+}
